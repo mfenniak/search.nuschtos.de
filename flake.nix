2f16b0f270076acc60716fd8624814e8e03cf969
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-08 22:55:04 +0200

    Add demo site

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "Flake for search.nüschtos.de";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      # url = "github:nix-community/home-manager";
+      # https://github.com/nix-community/home-manager/pull/5942
+      url = "github:SuperSandro2000/home-manager/nuschtos-search-options-json";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    search = {
+      url = "github:NuschtOS/search";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    nixos-modules = {
+      url = "github:NuschtOS/nixos-modules";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        search.follows = "search";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, home-manager, search, nixos-modules, nixvim, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
+        in
+        {
+          packages = {
+            default = search.packages.${system}.mkMultiSearch {
+              scopes = [
+                {
+                  optionsJSON = home-manager.packages.${system}.docs-html.passthru.home-manager-options.nixos + /share/doc/nixos/options.json;
+                  urlPrefix = "https://github.com/nix-community/home-manager/tree/master/";
+                }
+                {
+                  optionsJSON = home-manager.packages.${system}.docs-json + /share/doc/home-manager/options.json;
+                  optionsPrefix = "home-manager.users.<name>";
+                  urlPrefix = "https://github.com/nix-community/nixvim/tree/main/";
+                }
+                {
+                  modules = [
+                    ({ config, lib, ... }: {
+                      _module.args = {
+                        libS = nixos-modules.lib { inherit config lib; };
+                        inherit pkgs;
+                      };
+                      imports = [ (pkgs.path + "/nixos/modules/misc/extra-arguments.nix") ];
+                    })
+                    nixos-modules.nixosModule
+                  ];
+                  urlPrefix = "https://github.com/NuschtOS/nixos-modules/tree/main/";
+                }
+                {
+                  optionsJSON = nixvim.packages.${system}.options-json + /share/doc/nixos/options.json;
+                  optionsPrefix = "programs.nixvim";
+                  urlPrefix = "https://github.com/nix-community/nixvim/tree/main/";
+                }
+              ];
+            };
+          };
+        });
+}
